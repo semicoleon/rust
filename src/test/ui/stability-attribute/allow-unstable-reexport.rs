82b7fc22d2f1df41042b46b66905f12a13634ca2
cole <cole@swift-mail.com>
2022-05-29 17:47:18 -0500

    Add a test checking whether the feature gate on a re-exported item is ever triggered

---
+++ src/test/ui/stability-attribute/allow-unstable-reexport.rs
@@ -0,0 +1,20 @@
+// Allow an unstable re-export without requiring a feature gate. https://github.com/rust-lang/rust/issues/94972
+
+// aux-build:lint-stability.rs
+// aux-build:lint-stability-reexport.rs
+#![feature(staged_api)]
+#![feature(unstable_test_feature)]
+#![stable(feature = "lint_stability", since = "1.0.0")]
+
+extern crate lint_stability;
+extern crate lint_stability_reexport;
+
+use lint_stability::unstable;
+// We want to confirm that using a re-export through another crate behaves the same way as using an item directly
+use lint_stability_reexport::unstable_text;
+
+fn main() {
+    // Since we didn't enable the feature in this crate, we still can't use these items, even though they're in scope from the `use`s which are now allowed.
+    unstable(); //~ ERROR use of unstable library feature 'unstable_test_feature'
+    unstable_text(); //~ ERROR use of unstable library feature 'unstable_test_feature'
+}
---
+++ src/test/ui/stability-attribute/auxiliary/lint-stability-reexport.rs
@@ -0,0 +1,10 @@
+#![crate_type = "lib"]
+#![feature(staged_api)]
+#![feature(unstable_test_feature)]
+#![stable(feature = "lint_stability", since = "1.0.0")]
+
+extern crate lint_stability;
+
+// Re-exporting without enabling the feature "unstable_test_feature" in this crate
+#[unstable(feature = "unstable_reexport", issue = "none")]
+pub use lint_stability::unstable_text;
